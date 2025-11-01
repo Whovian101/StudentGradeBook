5b576adf74c099b9a0708a64065589aea4ca4854
Linda Luna <lindathihuynh@gmail.com>
2025-11-01 09:32:25 -0700

    Initial Commit

---
+++ Project103-Ch4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project103-Ch4
+//
+//  Created by Linda Luna on 11/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project103-Ch4/Project103_Ch4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project103_Ch4App.swift
+//  Project103-Ch4
+//
+//  Created by Linda Luna on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Project103_Ch4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
